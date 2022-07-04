5821298470b178a1a4140e0b9ef33ff7e3f937d9
Matiaso2194 <matiassorlando@gmail.com>
2022-07-04 23:11:06 +0200

    update img page 404

--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
-    <img
-      src="images/page404.jpg"
-      alt="Pagina no encontrada: Error 404"
-      style={{ width: "100%", height: "100vh" }}
-    />
+    <NavLink to="/home">
+      <img
+        src="images/page404.jpg"
+        alt="Pagina no encontrada: Error 404"
+        style={{ width: "100%", height: "100vh" }}
+      />
+    </NavLink>
   );
 };
 
